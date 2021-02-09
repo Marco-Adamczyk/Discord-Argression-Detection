3c4487d1dfce8d74d1484fb296ed5c840ab98121
Marco Adamczyk <marco.adamczyk@uni-bielefeld.de>
2021-02-09 21:24:48 +0100

    Added the noise from file method, pca, new net and improved code structure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import keras
 import os
-
+import copy
+import time
 import scipy
 from sklearn.decomposition import PCA
 from tensorflow.python.keras import Sequential
@@ -76,10 +77,6 @@ class RevdessDataset:
         return list(filter(lambda file: os.path.basename(file).split('-')[type_index] in values, query))
 
 
-def add_noise_from_file(x_data, y_data, noise_file, percent_of_data, max_pitch_vary, max_db_vary):
-
-
-
 def get_features(data_x):
     new_data_x = []
     for data in tqdm(data_x):
@@ -141,7 +138,27 @@ def duplicate_data(x_data, y_data, length=0.5, times=4):
     return x_new_data, y_new_data
 
 
-def vary_pitch(x_data, y_data, max_pitch_diff=3, new_pitch_per_data=3):
+def add_noise_from_file(x_data, y_data, noise_file, percent_of_data, max_pitch_vary, max_db_vary):
+    noise_signal, _ = librosa.load(noise_file, mono=True)
+    for i in range(len(x_data)):
+        x_data[i].append(y_data[i])
+    edit_x_data = random.sample(x_data, int(np.floor(len(x_data) * percent_of_data)))
+    new_x_data = []
+    new_y_data = []
+    for i in tqdm(range(len(edit_x_data))):
+        signal = edit_x_data[i][0]
+        sr = edit_x_data[i][1]
+        label = edit_x_data[i][2]
+        signal_start = random.randint(0, len(noise_signal) - len(signal) - 1)
+        edited_noise_signal = noise_signal[signal_start:signal_start + len(signal)]
+        edited_noise_signal = librosa.effects.pitch_shift(edited_noise_signal, sr,
+                                                          random.uniform(-max_pitch_vary, max_pitch_vary))
+        new_x_data.append([signal + edited_noise_signal, sr])
+        new_y_data.append(label)
+    return new_x_data, new_y_data
+
+
+def vary_pitch(x_data, y_data, max_pitch_diff=1, new_pitch_per_data=3):
     x_new_data = []
     y_new_data = []
     for i in tqdm(range(len(x_data))):
@@ -155,98 +172,96 @@ def vary_pitch(x_data, y_data, max_pitch_diff=3, new_pitch_per_data=3):
 
 
 def shuffle_data(x_data, y_data, times=20):
-    for i in tqdm(range(20)):
-        x_data, y_data = sklearn.utils.shuffle(x_complete_data, y_complete_data)
+    for i in tqdm(range(times)):
+        x_data, y_data = sklearn.utils.shuffle(x_data, y_data)
 
     return x_data, y_data
 
 
-x_complete_data = []
-y_complete_data = []
 angry_label = 1
 not_angry_label = 0
-x_train = []
-x_test = []
-y_train = []
-y_test = []
-
-# create workspace structure
-resource_path = os.path.join(os.getcwd(), 'resources')
-data_files_path = os.path.join(resource_path, 'data_files')
-if not os.path.isdir(resource_path):
-    os.mkdir(data_files_path)
-if not os.path.isdir(resource_path):
-    os.mkdir(data_files_path)
-
-if len(os.listdir(data_files_path)) == 0:
-    print("Get Ravdess dataset")
-    data_set = RevdessDataset(resource_path)
-
-    # debug
-    print(len(compute_feature_vector(librosa.load(data_set.files[0], mono=True), 12)))
-
-    print("Extract files with label: 1")
-    x, y = extract_data_from_file(data_set.query(emotion=['05', '08'], intensity=['02']), 1)
-    x_complete_data = x_complete_data + x
-    y_complete_data = y_complete_data + y
-
-    print("Extract files with label: 0")
-    x, y = random.sample(
-        extract_data_from_file(data_set.query(emotion=['01', '02', '03', '04', '05', '06'], intensity=['01', '02']), 0),
-        len(x_complete_data))
-    x_complete_data = x_complete_data + x
-    y_complete_data = y_complete_data + y
-
-    print("Duplicate data data set:")
-    x_complete_data, y_complete_data = duplicate_data(x_complete_data, y_complete_data)
-
-    print("Add some pitched examples:")
-    x, y = vary_pitch(x_complete_data, y_complete_data)
-    x_complete_data = x_complete_data + x
-    y_complete_data = y_complete_data + y
-
-    print("Add clean noisy data:")
-    x, y = add_noise_from_file(x_complete_data, y_complete_data, os.path.join(resource_path, "clean_noise.wav"), 1.0, 0.8, 10)
-    x_complete_data = x_complete_data + x
-    y_complete_data = y_complete_data + y
-
-    print("Add keyboard noise:")
-    x, y = add_noise_from_file(x_complete_data, y_complete_data, os.path.join(resource_path, "keyboard_sound.wav"), 0.1, 0.5, 10)
-    x_complete_data = x_complete_data + x
-    y_complete_data = y_complete_data + y
-
-    print("Add mouse click noise:")
-    x, y = add_noise_from_file(x_complete_data, y_complete_data, os.path.join(resource_path, "mouse_click_sound.wav"), 0.05, 0.5, 10)
-    x_complete_data = x_complete_data + x
-    y_complete_data = y_complete_data + y
 
 
-    print("Add error noise:")
+def get_processed_data(resource_path, data_files_path):
+    x_complete_data = []
+    y_complete_data = []
 
-    print("Compute features from signal:")
-    x_complete_data = get_features(x_complete_data)
+    if len(os.listdir(data_files_path)) == 0:
+        print("Get Ravdess dataset")
+        data_set = RevdessDataset(resource_path)
 
-    print("Shuffle data:")
-    x_complete_data, y_complete_data = shuffle_data(x_complete_data, y_complete_data, 200)
+        # debug
+        print(len(compute_feature_vector(librosa.load(data_set.files[0], mono=True), 12)))
 
-    print("Separate test set from data(80/20)")
-    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x_complete_data, y_complete_data,
-                                                                                test_size=0.2)
+        print("Extract files with label: 1")
+        x, y = extract_data_from_file(data_set.query(emotion=['05', '08'], intensity=['02']), 1)
+        x_complete_data = x_complete_data + x
+        y_complete_data = y_complete_data + y
 
-    print("Save as csv")
-    os.chdir(data_files_path)
-    np.save('x_train', np.asarray(x_train), True)
-    np.save('y_train', np.asarray(y_train), True)
-    np.save('x_test', np.asarray(x_test), True)
-    np.save('y_test', np.asarray(y_test), True)
-    os.chdir("../..")
-else:
-    os.chdir(data_files_path)
-    x_train = np.load('x_train.npy', mmap_mode=None, allow_pickle=True)
-    y_train = np.load('y_train.npy', mmap_mode=None, allow_pickle=True)
-    x_test = np.load('x_test.npy', mmap_mode=None, allow_pickle=True)
-    y_test = np.load('y_test.npy', mmap_mode=None, allow_pickle=True)
-    os.chdir("../..")
+        print("Extract files with label: 0")
+        x, y = extract_data_from_file(
+            random.sample(data_set.query(emotion=['01', '02', '03', '04', '05', '06'], intensity=['01', '02'])
+                          , len(x_complete_data)), 0)
+        x_complete_data = x_complete_data + x
+        y_complete_data = y_complete_data + y
+
+        time.sleep(1)
+        print("Duplicate data data set:")
+        time.sleep(1)
+        x_complete_data, y_complete_data = duplicate_data(x_complete_data, y_complete_data)
+        time.sleep(1)
+
+        print("Add some pitched examples:")
+        x, y = vary_pitch(copy.deepcopy(x_complete_data), copy.deepcopy(y_complete_data))
+        x_complete_data = x_complete_data + x
+        y_complete_data = y_complete_data + y
+
+        print("Add clean noisy data:")
+        x, y = add_noise_from_file(copy.deepcopy(x_complete_data), copy.deepcopy(y_complete_data), os.path.join(resource_path, "clean_noise.wav"), 1.0,
+                                   0.8, 10)
+        print(len(x_complete_data[0]))
+        x_complete_data = x_complete_data + x
+        y_complete_data = y_complete_data + y
+
+        print("Add keyboard noise:")
+        x, y = add_noise_from_file(copy.deepcopy(x_complete_data), copy.deepcopy(y_complete_data), os.path.join(resource_path, "keyboard_sound.wav"), 0.1,
+                                   0.5, 10)
+        x_complete_data = x_complete_data + x
+        y_complete_data = y_complete_data + y
+
+        print("Add mouse click noise:")
+        x, y = add_noise_from_file(copy.deepcopy(x_complete_data), copy.deepcopy(y_complete_data), os.path.join(resource_path, "mouse_click_sound.wav"),
+                                   0.05, 0.5, 10)
+        x_complete_data = x_complete_data + x
+        y_complete_data = y_complete_data + y
+
+        print("Compute features from signal:")
+        x_complete_data = get_features(x_complete_data)
+
+        print("Shuffle data:")
+        x_complete_data, y_complete_data = shuffle_data(x_complete_data, y_complete_data, 200)
+
+        print("Separate test set from data(80/20)")
+        x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x_complete_data, y_complete_data,
+                                                                                    test_size=0.2)
+
+        print("Save as csv")
+        os.chdir(data_files_path)
+        np.save('x_train', np.asarray(x_train), True)
+        np.save('y_train', np.asarray(y_train), True)
+        np.save('x_test', np.asarray(x_test), True)
+        np.save('y_test', np.asarray(y_test), True)
+        os.chdir("../..")
+    else:
+        print("Load csv data")
+        os.chdir(data_files_path)
+        x_train = np.load('x_train.npy', mmap_mode=None, allow_pickle=True)
+        y_train = np.load('y_train.npy', mmap_mode=None, allow_pickle=True)
+        x_test = np.load('x_test.npy', mmap_mode=None, allow_pickle=True)
+        y_test = np.load('y_test.npy', mmap_mode=None, allow_pickle=True)
+        os.chdir("../..")
+
+    return x_train, y_train, x_test, y_test
 
 
 def evaluate_model(trainX, trainy, testX, testy):
@@ -296,12 +311,24 @@ def summarize_results(scores):
     print('Accuracy: %.3f%% (+/-%.3f)' % (m, s))
 
 
-# pca select main features
-pca = PCA(.95)
+# create workspace structure
+resource_path = os.path.join(os.getcwd(), 'resources')
+data_files_path = os.path.join(resource_path, 'data_files')
+if not os.path.isdir(resource_path):
+    os.mkdir(resource_path)
+if not os.path.isdir(data_files_path):
+    os.mkdir(data_files_path)
 
+x_train, y_train, x_test, y_test = get_processed_data(resource_path, data_files_path)
+# pca select main features
+take_variance = .99
+pca = PCA(take_variance)
+
+print("Compute pca relevant features with " + str(take_variance) +  " percent of variance")
+previous_dims = len(x_train[0])
 train_pca = pca.fit_transform(x_train)
 test_pca = pca.transform(x_test)
-
+print(str(len(train_pca[0])) + " dims are used from initially " + str(previous_dims))
 # expand dims
 x_train = np.expand_dims(x_train, axis=2)
 x_test = np.expand_dims(x_test, axis=2)
